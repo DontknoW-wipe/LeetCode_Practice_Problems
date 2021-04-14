9129993cb84840524da48fc66783a57b13176a80
DontknoW-wipe <57296632+DontknoW-wipe@users.noreply.github.com>
2021-04-14 15:01:47 +0530

    Create code.java

---
+++ 27. Remove Element/code.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int size=nums.length;
+        int i,j;
+        for(i=0,j=0;j<size;j++)
+        {
+            if(nums[j]!=val)
+            {
+                nums[i]=nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
